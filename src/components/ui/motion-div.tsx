0e5cfd30c515718ed328c47faf680f1cd4d6ca6e
Arun Kumar <arun.kumar.cd.civ21@itbhu.ac.in>
2025-06-19 22:33:17 +0530

    fixing route-3

--- src/components/ui/motion-div.tsx
+++ src/components/ui/motion-div.tsx
@@ -1,4 +1,10 @@
-// components/motion-div.tsx
+// components/ui/motion-div.tsx
+"use client";
 import { motion } from "framer-motion";
+import { ComponentProps } from "react";
 
-export const MotionDiv = motion.div;
+type MotionDivProps = ComponentProps<typeof motion.div>;
+
+export const MotionDiv = ({ children, ...props }: MotionDivProps) => {
+    return <motion.div {...props}>{children}</motion.div>;
+};
