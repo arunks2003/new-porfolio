b0cae0467bfddc18424ccda74d439291af908b20
Arun Kumar <arun.kumar.cd.civ21@itbhu.ac.in>
2025-06-19 22:21:44 +0530

    fixed-route-2

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 import VerificationEmail from '@/emails/VerificationEmail';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
